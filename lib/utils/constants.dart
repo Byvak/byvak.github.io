da5b69b327733a17159a3e87a2498272ab83fd8e
Burnny Kibonge <kibonge.burnny@gmail.com>
2021-12-24 13:39:03 +0100

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xFFFFFFFF);
+const blackColor = Color(0xFF000000);
+const textColor = Color(0xFF1D150B);
+const primaryColor = Color(0xFFFB475F);
+const secondaryColor = Color(0xFFF5E1CB);
+const borderColor = Color(0xFFDDDDDD);
